5e9b02527b9f69d8bc6ca73e15be30e4438b238f
Taehyeon <taehyeon@Taehyeons-MacBook-Pro.local>
2024-03-05 14:00:15 +0900

    Task2 complete until step5f.

--- app/player.py
+++ app/player.py
@@ -1,7 +1,11 @@
+from argon2 import PasswordHasher
+
 class Player:
     def __init__(self, id: str, player_name: str):
         self.id = id
         self.player_name = player_name
+        self.ph = PasswordHasher()
+        self._hashed_password = None
 
     def uid(self):
         return self.id
@@ -12,6 +16,17 @@ class Player:
     def __str__(self):
         return f"Id:{self.id}  Name:{self.player_name}"
 
+    def add_password(self, password: str):
+        self._hashed_password = self.ph.hash(password)
+
+    def verify_password(self, password_to_verify: str):
+        if self.ph.verify(self._hashed_password, password_to_verify):
+            return True
+        return False
+
 
 player_1 = Player("100", "John")
 player_2 = Player("101", "David")
+
+player_1.add_password("hello")
+print(player_1.verify_password("hello"))
--- test/player_test.py
+++ test/player_test.py
@@ -15,5 +15,18 @@ class TestTheProperties(unittest.TestCase):
         self.assertIsInstance(self.player_temp.player_name, str)
 
 
+class TestPasswordVerification(unittest.TestCase):
+    def setUp(self):
+        self.player_temp = Player("104", "Mike")
+
+    def test_password_verification(self):
+        self.player_temp.add_password("a1b2c3d4")
+        self.assertEqual(self.player_temp.verify_password("a1b2c3d4"), True)
+
+    def test_password_verification_with_longer_password_input(self):
+        self.player_temp.add_password("a29vkdm20sx2x03kfm84ifmv03wwwlw03")
+        self.assertEqual(self.player_temp.verify_password("a29vkdm20sx2x03kfm84ifmv03wwwlw03"), True)
+
+
 if __name__ == "__main__":
     unittest.main()
