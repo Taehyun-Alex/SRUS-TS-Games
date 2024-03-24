61560bbccc0b8c06a4dff0df9fd767937bd75f8d
Taehyun Sung <alexseong1004@gmail.com>
2024-03-24 09:21:37 +0800

    continue on static sorting method

--- app/player.py
+++ app/player.py
@@ -2,11 +2,12 @@ from argon2 import PasswordHasher
 
 
 class Player:
-    def __init__(self, id: str, player_name: str):
+    def __init__(self, id: str, player_name: str, score: int = None):
         self.id = id
         self.player_name = player_name
         self.ph = PasswordHasher()
         self._hashed_password = None
+        self._score = score
 
     def uid(self):
         return self.id
@@ -14,8 +15,26 @@ class Player:
     def name(self):
         return self.player_name
 
+    def get_score(self):
+        return self._score
+
+    def set_score(self, new_score):
+        if new_score >= 1 and isinstance(new_score, int):
+            self._score = new_score
+        else:
+            raise ValueError("Score must be a positive integer value.")
+
     def __str__(self):
         return f"Id:{self.id}  Name:{self.player_name}"
+
+    def __eq__(self, other):
+        return self._score == other.get_score()
+
+    def __gt__(self, other):
+        return self._score > other.get_score()
+
+    def __lt__(self, other):
+        return self._score < other.get_score()
 
     def add_password(self, password: str):
         self._hashed_password = self.ph.hash(password)
@@ -24,6 +43,12 @@ class Player:
         if self.ph.verify(self._hashed_password, password_to_verify):
             return True
         return False
+    @staticmethod
+    def sort_in_descending_order(lst):
+        # write quick sort algorithm in descending order
+
+
+
 
 
 player_1 = Player("100", "John")
--- test/player_test.py
+++ test/player_test.py
@@ -28,5 +28,22 @@ class TestPasswordVerification(unittest.TestCase):
         self.assertEqual(self.player_temp.verify_password("a29vkdm20sx2x03kfm84ifmv03wwwlw03"), True)
 
 
+class TestScoreComparisonOperators(unittest.TestCase):
+
+    def setUp(self):
+        self.player_1 = Player("105", "Michael", 5)
+        self.player_2 = Player("106", "Layla", 2)
+        self.player_3 = Player("107", "Bec", 2)
+
+    def test_equal_to(self):
+        self.assertEqual(self.player_2 == self.player_3, True)
+
+    def test_greater_than(self):
+        self.assertEqual(self.player_1 > self.player_2, True)
+
+    def test_less_than(self):
+        self.assertEqual(self.player_3 < self.player_1, True)
+
+
 if __name__ == "__main__":
     unittest.main()
