55a4aad895907ddfa6e08431637c8c01b97970d1
Taehyun Sung <20101299@tafe.wa.edu.au>
2024-04-03 08:48:15 +0800

    continue on insert BST node method

--- app/player_bst.py
+++ app/player_bst.py
@@ -5,3 +5,20 @@ class PlayerBST:
     @property
     def root(self):
         return self._root
+
+    def insert(self, player):
+        if self._root is None:
+            self._root = player.player_name
+        else:
+            if self._root > player.player_name:
+                if self._root.left is None:
+                    self._root.left = player.player_name
+                else:
+                    # Resursive
+            elif self._root < player.player_name:
+                if self._root.right is None:
+                    self._root.right = player.player_name
+                else:
+                    # Recursive
+            else:
+                # same name, so update the value
