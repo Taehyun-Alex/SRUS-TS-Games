accaf403454d607935bfba73af84504bc4d83398
TAEHYUN SUNG <SUNGTA@tdm.local>
2024-06-18 17:46:56 +0800

    Keep on fixing the assessment. Refer to notes.

--- app/player.py
+++ app/player.py
@@ -1,52 +1,64 @@
+from typing import Union, List, Optional
+
 from argon2 import PasswordHasher
 
 
 class Player:
-    def __init__(self, id: str, player_name: str, score: int = None):
+    def __init__(self, id: str, player_name: str, score: int = None) -> None:
         self.id = id
         self.player_name = player_name
         self.ph = PasswordHasher()
         self._hashed_password = None
         self._score = score
 
-    def uid(self):
+    def uid(self) -> str:
         return self.id
 
-    def name(self):
+    def name(self) -> str:
         return self.player_name
 
-    def get_score(self):
+    @property
+    def score(self) -> Optional[int]:
         return self._score
 
-    def set_score(self, new_score):
-        if new_score >= 1 and isinstance(new_score, int):
+    @score.setter
+    def score(self, new_score: Optional[int]) -> None:
+        if new_score is not None and new_score >= 1 and isinstance(new_score, int):
             self._score = new_score
         else:
             raise ValueError("Score must be a positive integer value.")
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Id:{self.id}  Name:{self.player_name}"
 
-    def __eq__(self, other):
-        return self._score == other.get_score()
+    def __eq__(self, other) -> bool:
+        return self.score == other.score
 
-    def __gt__(self, other):
-        return self._score > other.get_score()
+    def __gt__(self, other) -> bool:
+        return self.score > other.score
 
-    def __lt__(self, other):
-        return self._score < other.get_score()
+    def __lt__(self, other) -> bool:
+        return self.score < other.score
 
-    def add_password(self, password: str):
-        self._hashed_password = self.ph.hash(password)
+    @property
+    def password(self) -> Optional[str]:
+        return self._hashed_password
 
-    def verify_password(self, password_to_verify: str):
-        if self.ph.verify(self._hashed_password, password_to_verify):
+    @password.setter
+    def password(self, password: Optional[str]) -> None:
+        if password is not None:
+            self.password = self.ph.hash(password)
+        else:
+            raise ValueError("Password cannot be None.")
+
+    def verify_password(self, password_to_verify: str) -> bool:
+        if self.ph.verify(self.password, password_to_verify):
             return True
         return False
 
     @staticmethod
-    def radix_sort_in_descending_order(lst):
-        max_num = max(lst)
+    def radix_sort_in_descending_order(lst: List[int]) -> List[int]:
+        max_num = max(lst) if lst else 0
         max_digits = len(str(max_num))
 
         for digit_place in range(max_digits):
@@ -59,4 +71,3 @@ class Player:
             lst = [num for bucket in reversed(buckets) for num in bucket]
 
         return lst
-
--- app/player_node.py
+++ app/player_node.py
@@ -1,32 +1,37 @@
+from typing import Optional
+
+from player import Player
+
+
 class PlayerNode:
-    def __init__(self, player):
+    def __init__(self, player: Player) -> None:
         self._player = player
-        self._next = None
-        self._previous = None
+        self._next: Optional["PlayerNode"] = None
+        self._previous: Optional["PlayerNode"]= None
 
     # player getter
-    def get_player(self):
+    def get_player(self) -> Player:
         return self._player
 
     # next_node setter
-    def next_node(self, node):
+    def next_node(self, node) -> None:
         self._next = node
 
     # next_node getter
-    def get_next(self):
+    def get_next(self) -> Player:
         return self._next
 
     # previous_node setter
-    def previous_node(self, node):
+    def previous_node(self, node) -> None:
         self._previous = node
 
     # previous_node getter
-    def get_previous(self):
+    def get_previous(self) -> Player:
         return self._previous
 
     # returns uid property of the player instance
-    def key(self):
-        return self._player.uid
+    def key(self) -> str:
+        return self._player.uid()
 
-    def __str__(self):
+    def __str__(self) -> str:
         return str(self._player)
